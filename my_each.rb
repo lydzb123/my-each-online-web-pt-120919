ff8a9b478b38d500735c679c05fc3faa8e6e82f2
Lydia B <lydiabaik@gmail.com>
2019-12-17 08:25:36 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
 end
+
+
+my_each` method. This method should accept an argument of an array and use the `while` loop to iterate over each member of that array, yielding each element contained in the array to a block.
+
+Here's an example of what should happen when you call your `my_each` method:
+
+```ruby
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
